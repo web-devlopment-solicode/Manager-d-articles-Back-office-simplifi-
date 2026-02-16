2f1e7d119159ec5c0db969345c5d1857e460dae2
mahfoudeoumaima-ui <mahfoudeoumaima@gmail.com>
2026-02-16 10:44:24 +0100

    Add files via upload

---
+++ Chapitre 8 – Constructeurs, héritage et visibilité/BlogArticle.php
@@ -0,0 +1,14 @@
+<?php
+require 'article.php';
+class BlogArticle extends Article {
+    private $auteur;
+
+    public function __construct($titre, $contenu, $auteur) {
+        parent::__construct($titre, $contenu);
+        $this->auteur = $auteur;
+    }
+
+    public function afficher() {
+        return parent::afficher() . " - Auteur : " . $this->auteur;
+    }
+}
---
+++ Chapitre 8 – Constructeurs, héritage et visibilité/test.php
@@ -0,0 +1,5 @@
+<?php
+require 'BlogArticle.php';
+
+$article = new BlogArticle("POO en PHP", "Découvrir l'héritage.", "Alice");
+echo $article->afficher();
